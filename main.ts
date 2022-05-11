eaea850d042385afeb807fe3b5ee2db5054ce59c
Sandorex <rzhw3h@gmail.com>
2022-05-11 16:15:53 +0200

    Initial working version Few more tweaks like adding settings and it's ready

--- main.ts
+++ main.ts
@@ -1,137 +1,69 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Modal, Plugin, TFile } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
-	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
+export class TrashFilesModal extends Modal {
+	files: TFile[];
+	constructor(app: App, files: TFile[]) {
 		super(app);
+		this.files = files;
 	}
 
 	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		let { contentEl, titleEl } = this;
+		titleEl.setText("Move " + this.files.length + " files to obsidian trash?");
+
+		this.files.forEach(file => contentEl.createEl("p", {
+			cls: "trash-file-modal-text",
+			text: file.path
+		}));
+
+		contentEl.createEl("button", { text: "Cancel" })
+			.addEventListener("click", () => this.close());
+
+		contentEl.createEl("button", {
+			cls: ["mod-cta", "modal-confirm-button"],
+			text: "Confirm"
+		}).addEventListener("click", async () => {
+			for (const file of this.files) {
+				await this.app.vault.trash(file, false);
+			}
+			this.close();
+		});
 	}
 
 	onClose() {
-		const {contentEl} = this;
+		let { contentEl } = this;
 		contentEl.empty();
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+export default class NukeOrphansPlugin extends Plugin {
+	nuke() {
+		// TODO: still messy but it just gets all linked files ignoring linkers
+		// set containing linked files, each file has only one entry
+		var links = new Set<String>(Object.entries(this.app.metadataCache.resolvedLinks).flatMap(x => Object.entries(x[1]).map(y => y[0])));
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
+		var filesToDelete = this.app.vault.getFiles().filter(file => {
+			// TODO: find a way to get attachments folder and just use settings for now..
+			return file.parent.name == "@Attachments"
+				// TODO: do the resolved links use abs path? if not this may fail
+				&& !links.has(file.path)
+		});
+
+		if (filesToDelete.length > 0)
+			new TrashFilesModal(this.app, filesToDelete).open();
 	}
 
-	display(): void {
-		const {containerEl} = this;
+	async onload() {
+		console.log("Loaded plugin " + this.manifest.name + " " + this.manifest.version);
 
-		containerEl.empty();
+		this.addCommand({
+			id: 'nuke-orphans',
+			name: 'Move orphaned attachments into trash',
+			callback: () => this.nuke(),
+		});
+	}
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+	onunload() {
+		console.log("Unoaded plugin " + this.manifest.name + " " + this.manifest.version);
 	}
 }
